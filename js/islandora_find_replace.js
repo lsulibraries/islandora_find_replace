ff3ee3c32604a3fc9cfc2bb9f16b53d79a0559d0
Will <wwconlin@gmail.com>
2018-09-26 15:55:25 -0500

    selection working in jquery, conditionals set up.

--- js/islandora_find_replace.js
+++ js/islandora_find_replace.js
@@ -4,3 +4,24 @@
  */
 
 maxInputVars = Drupal.settings.islandora_find_replace.maxInputVars;
+
+
+
+(function ($) {
+  $('.select-all').change(function(){
+      console.log('ok');
+        if($('.form-checkbox').parent().parent().parent('.selected').length >= maxInputVars){
+          console.log('tooooo many');
+      }
+    });
+
+  $('.form-checkbox').change(function(){
+    if(this.checked) {
+        if($('.form-checkbox').parent().parent().parent('.selected').length >= maxInputVars){
+          console.log('tooooo many');
+        }
+
+      }
+
+  });
+}(jQuery));
