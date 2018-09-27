a60981de68ae3c0fc52fca5bd320572fc640e3d7
Will <wwconlin@gmail.com>
2018-09-27 11:37:17 -0500

    use .submit() with .preventDefault() and .unbind(), change message from status to error

--- js/islandora_find_replace.js
+++ js/islandora_find_replace.js
@@ -3,61 +3,46 @@
  * check if max_input_vars is met on form.
  */
 
-maxInputVars = 2;//Drupal.settings.islandora_find_replace.maxInputVars;
+maxInputVars = Drupal.settings.islandora_find_replace.maxInputVars;
 message = `You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`;
-markup = `<div id="console" class="clearfix"><div id="find_replace_warning" class="messages status"><h2 class="element-invisible">Status message</h2><pre>${message}</pre></div></div>`;
+markup = `<div id="console" class="clearfix"><div id="find_replace_warning" class="messages error"><h2 class="element-invisible">Status message</h2><pre>${message}</pre></div></div>`;
 
 (function ($) {
   Drupal.behaviors.tooManyMessage = {
     attach: function (context, settings) {
       $.fn.tooManySelected = function() {
-        console.log('we running');
         selected = this.parent().parent().parent('.selected').length;
         messageExists = $('#find_replace_warning').length;
-        if(selected < maxInputVars){
+
+        if (selected < maxInputVars){
           if (messageExists) {
             $('#find_replace_warning').parent().html('');
-            console.log('replace while message exists')
+            $("form").submit(function(){
+              $(this).unbind('submit').submit()
+            });
           }
         }
-      else {
-        if(!messageExists){
-          $('#content.clearfix').prepend(markup);
-          console.log('prepending while message doesn"t exist');
-          console.log(selected);
+
+        else {
+          if(!messageExists){
+            $('#content.clearfix').prepend(markup);
+            $("form").submit(function(e){
+              e.preventDefault();
+            });
+          }
         }
       }
+
+      $('.select-all').change(function(){
+        $('.form-checkbox').tooManySelected();
+      });
+
+      $('.form-checkbox').change(function(){
+        if(this.click) {
+          $('.form-checkbox').tooManySelected();
+        }
+      });
+
     }
   }
-
-  $('.select-all').change(function(){
-        $('.form-checkbox').tooManySelected();
-    });
-
-  $('.form-checkbox').change(function(){
-    if(this.click) {
-        $('.form-checkbox').tooManySelected();
-      }
-
-  });
 }(jQuery));
-
-// Drupal.behaviors.tooManyMessage = {
-//   attach: function (context, settings) {
-//     alert(`You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`);
-//   }
-// }
-
-// (function ($) {
-//   $.fn.tooManySelected = function() {
-//     if(this.parent().parent().parent('.selected').length >= maxInputVars){
-//       //Drupal.behaviors.tooManyMessage = {
-//         // attach: function (context, settings) {
-//           // $('input.tooManyMessage', context).once('tooManyMessage', function () {
-//             alert(`You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`);
-//
-//           // });
-//         }
-//       //}
-//     }
-//   }
