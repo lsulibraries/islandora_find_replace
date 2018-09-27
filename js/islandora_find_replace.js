c68e1b6d86c598735741ddf002d4a9a7ad9a1b8f
Will <wwconlin@gmail.com>
2018-09-27 08:59:30 -0500

    alert message prep, seeking to match markup of drupal_set_message in jquery behavior, trying something

--- js/islandora_find_replace.js
+++ js/islandora_find_replace.js
@@ -4,11 +4,19 @@
  */
 
 maxInputVars = Drupal.settings.islandora_find_replace.maxInputVars;
+message = `You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`;
+html = `<div class="messages status"><h2 class="element-invisible">Status message</h2><pre>${message}</pre></div>`;
 
 (function ($) {
-  $.fn.tooManySelected = function() {
-    if(this.parent().parent().parent('.selected').length >= maxInputVars){
-      console.log('too_many');
+  Drupal.behaviors.tooManyMessage = {
+    attach: function (context, settings) {
+      $.fn.tooManySelected = function() {
+        if(this.parent().parent().parent('.selected').length >= maxInputVars){
+          //alert(message);
+          $('#console.clearfix').val(html);
+          console.log($('#console').val());
+        }
+      }
     }
   }
 
@@ -23,3 +31,23 @@ maxInputVars = Drupal.settings.islandora_find_replace.maxInputVars;
 
   });
 }(jQuery));
+
+// Drupal.behaviors.tooManyMessage = {
+//   attach: function (context, settings) {
+//     alert(`You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`);
+//   }
+// }
+
+// (function ($) {
+//   $.fn.tooManySelected = function() {
+//     if(this.parent().parent().parent('.selected').length >= maxInputVars){
+//       //Drupal.behaviors.tooManyMessage = {
+//         // attach: function (context, settings) {
+//           // $('input.tooManyMessage', context).once('tooManyMessage', function () {
+//             alert(`You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`);
+//
+//           // });
+//         }
+//       //}
+//     }
+//   }
