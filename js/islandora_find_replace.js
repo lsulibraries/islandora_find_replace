950c4b04f1f5a3f92dc598e25a503ff8f71ef4d3
Will <wwconlin@gmail.com>
2018-09-27 10:17:58 -0500

    logic working great, no prepend accumulation, does clear out other dsm messages..

--- js/islandora_find_replace.js
+++ js/islandora_find_replace.js
@@ -5,7 +5,7 @@
 
 maxInputVars = Drupal.settings.islandora_find_replace.maxInputVars;
 message = `You have selected more than the php variable max_input_vars will allow. Don't select over ${maxInputVars} items in the form!`;
-markup = `<div id="console" class="clearfix"><div id="#find_replace_warning" class="messages status"><h2 class="element-invisible">Status message</h2><pre>${message}</pre></div></div>`;
+markup = `<div id="console" class="clearfix"><div id="find_replace_warning" class="messages status"><h2 class="element-invisible">Status message</h2><pre>${message}</pre></div></div>`;
 
 (function ($) {
   Drupal.behaviors.tooManyMessage = {
@@ -13,18 +13,17 @@ markup = `<div id="console" class="clearfix"><div id="#find_replace_warning" cla
       $.fn.tooManySelected = function() {
         selected = this.parent().parent().parent('.selected').length;
         if (selected >= maxInputVars) {
-          if ($('#find_replace_warning').length) {
-            console.log('replace');
-            $('#find_replace_warning').html('');
+          if ( $('#find_replace_warning').length ) {
+            if(!selected >= maxInputVars){
+              $('#find_replace_warning').parent().html('');
+            }
           }
           else {
-            console.log('prepend');
             $('#content.clearfix').prepend(markup);
           }
           console.log(selected);
         }
         else {
-          console.log('not_greater_than');
           $('#console.clearfix').html('');
         }
       }
